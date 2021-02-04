046b681fbf1591622c99007339b2add48d6b2c82
Ethan Huston <ethanghuston@gmail.com>
2021-02-03 21:29:34 -0500

    Creating tank and turret

--- Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xD3D3E0E5,
+				0x222BB16B,
 				0x1228C189,
 				METADATA_PARAMS(nullptr, 0)
 			};
