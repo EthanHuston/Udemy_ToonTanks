807631e563db1087c949d6126be8ddf920bfb20b
Ethan Huston <ethanghuston@gmail.com>
2021-02-05 15:12:09 -0500

    Adding SpringArm and Camera pt3

--- Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x0414AA7B,
-				0xCDE2FDA6,
+				0x222BB16B,
+				0x1228C189,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
