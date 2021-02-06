a8eef658d9ba80256e2b5eff0efc4f233f438ddc
Ethan Huston <ethanghuston@gmail.com>
2021-02-06 10:23:35 -0500

    Implementing movement pt2

--- Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/Pawns/PawnTank.h
+++ Toon Tanks/ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/Pawns/PawnTank.h
@@ -21,6 +21,19 @@ class TOONTANKS_API APawnTank : public APawnBase
 		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		UCameraComponent* Camera;
 
+		FVector MoveDirection;
+		FQuat RotationDirection;
+
+		float MoveSpeed = 100.0f;
+		float RotateSpeed = 100.0f;
+
+		void CalculateMoveInput(float Value);
+		void CalculateRotateInput(float Value);
+
+		void Move();
+		void Rotate();
+
+
 	public:
 
 		APawnTank();
